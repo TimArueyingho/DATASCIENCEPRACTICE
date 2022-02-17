1046eacc44ac006a9732666046650e8e617a1a63
Tim_Arueyingho <tim.arueyingho@bristol.ac.uk>
2022-02-17 16:30:02 +0000

    UPDATE

--- numpytutorial.py
+++ numpytutorial.py
@@ -1,6 +1,8 @@
 import numpy as np
+import random
 
 #Imagine arrays as stacks
+#0/1/y/x/column/row
 #1D
 a = np.array([1, 2, 3, 4, 5, 6])
 print(a)
@@ -67,4 +69,46 @@ print (y)
 #we want the array to be full of 6, as 4 of axis 0 and 3 of axis 1
 fulls = np.full((4,3), 6)
 print(fulls)
+
+#we can add a particular number, diagonally
+#(axis 0= 3, axis 1 = 7, the diagonals would be 1...(x,y,z)...z states the index from which the diagonal would start
+identity = np.eye(3,7,1)
+print(identity)
+
+#generate random numbers in an array with the specified shape
+randoms = np.random.rand(4,3)
+print(randoms)
+
+# Create an array of random integers
+#we specify the highest number of the integer we want and the shape of the array (axis 0/y = 4, axis 1/x = 3
+randInts = np.random.randint(10, size = (4,3)) # The syntax requires we specify␣
+print(randInts)
+
+
+#let us slice the array, generate the first two columns
+print (randInts[0:2])
+
+#let us slice the array, generate the last two columns
+print(randInts[-2:])
+
+#let us slice it vertically, and pull out the last two columns instead of rows
+#(axis 0/y, axis1/x...0..by putting the comma , we are separating them into axes
+#we need index 1 till the end.. zero is the first column we are ignoring
+
+print (randInts[:,1:])
+
+#we could do some maths on numpy arrays #they must be the same shape
+x = np.array([[53, 74],
+              [2, 14]])
+y = np.array([[31.3, 0.8],[12.7, 8.1]])
+
+print (x + y)
+print (x - y)
+print(np.sqrt(x)) #square root of x
+
+#calculate the sum of the values in just the column (axis = 0)
+print(np.sum(x, axis = 0))
+
+#calculate the sum of the values in just the row (axis = 1)
+print (np.sum(x, axis = 1))
 
